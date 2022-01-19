4377e9a1b84596dd17913abd7ee6d63a25b30889
Kyle Davis <kyledavis109@gmail.com>
2022-01-19 13:55:59 -0600

    addition of try catch statements in server

--- express.js
+++ express.js
@@ -22,38 +22,53 @@ app.get('/api/albums/:artistID', async (req, res) => {
         if ('error' in results) {
             const error = results.error.message
             return res.status(400).send(error)
-        } 
-        return res.status(200).json(results.items)
+        }
+        const albumNames = [];
+        const albums = results.items.filter((item) => {
+            if (!albumNames.includes(item.name)) {
+                albumNames.push(item.name)
+                return item
+            }
+        })
+        return res.status(200).json(albums)
     } catch (error) {
-        return res.status(500).send('Failed to fetch albums.')
-    }
-})
+        return res.status(500).send('Server failed to fetch artist albums data.')
+    };
+});
 
 app.get('/api/relatedArtists/:artistID', async (req, res) => {
-    const artistID = req.params.artistID
-    const url = `https://api.spotify.com/v1/artists/${artistID}/related-artists`;
-    const response = await fetch(url, { 
-      headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
-      }
-    });
-    const results = await response.json();
-    return res.status(200).json(results)
-})
-
-app.get('/api/tracks/:albumID', async (req, res) => {
-    const albumID = req.params.albumID
-    console.log(albumID)
-    const url = `https://api.spotify.com/v1/albums/${albumID}/tracks`;
-    const response = await fetch(url, { 
+    try{
+        const artistID = req.params.artistID
+        const url = `https://api.spotify.com/v1/artists/${artistID}/related-artists`;
+        const response = await fetch(url, { 
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
         }
-    });
-    const results = await response.json();
-    return res.status(200).json(results)
+        });
+        const results = await response.json();
+        return res.status(200).json(results)
+    } catch(err) {
+        return res.status(500).send('Server failed to fetch related related artists data.')
+    }
+})
+
+app.get('/api/tracks/:albumID', async (req, res) => {
+    try{
+        const albumID = req.params.albumID
+        console.log(albumID)
+        const url = `https://api.spotify.com/v1/albums/${albumID}/tracks`;
+        const response = await fetch(url, { 
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+            }
+        });
+        const results = await response.json();
+        return res.status(200).json(results)
+    } catch(err) {
+        return res.status(500).send('Server failed to fetch artist album tracks.')
+    }
 })
 
 // Start the server listening for requests.
